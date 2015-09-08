0056711d6d7191c384e9f3a58966e9dd8da9aacc
toxick <aaron.segura@gmail.com>
2015-09-08 12:47:48 +0000

    Added keyerror

--- library/credentials.py
+++ library/credentials.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
-import os
+import os, sys
 
 def get_credentials():
     d = {}
-    d['username'] = os.environ['OS_USERNAME']
-    d['password'] = os.environ['OS_PASSWORD']
-    d['auth_url'] = os.environ['OS_AUTH_URL']
-    d['tenant_name'] = os.environ['OS_TENANT_NAME']
-    return d
+    try:
+        d['username'] = os.environ['OS_USERNAME']
+        d['password'] = os.environ['OS_PASSWORD']
+        d['auth_url'] = os.environ['OS_AUTH_URL']
+        d['tenant_name'] = os.environ['OS_TENANT_NAME']
+        return d
+    except KeyError, e:
+        print ("Environment variable %s is not set!") % e
+        sys.exit(1)
 
 def get_nova_credentials():
     d = {}
-    d['username'] = os.environ['OS_USERNAME']
-    d['api_key'] = os.environ['OS_PASSWORD']
-    d['auth_url'] = os.environ['OS_AUTH_URL']
-    d['project_id'] = os.environ['OS_TENANT_NAME']
-    return d
+    try:
+        d['username'] = os.environ['OS_USERNAME']
+        d['api_key'] = os.environ['OS_PASSWORD']
+        d['auth_url'] = os.environ['OS_AUTH_URL']
+        d['project_id'] = os.environ['OS_TENANT_NAME']
+        return d
+    except KeyError, e:
+        print ("Environment variable %s is not set!") % e
+        sys.exit(1)
